40382b3c59978241d141deddbfdb026ce32f7c8b
Pandiablo <malki.mehdi@free.fr>
2012-05-26 23:00:51 +0300

    Update id.php

--- id.php
+++ id.php
@@ -25,14 +25,7 @@ body {
 <center><a href="index.php"><img src="http://img34.imageshack.us/img34/4835/mg3h.jpg" alt="Logo MG" style="margin-bottom:60px;" /></a></center>
 
 <div style="margin-left:100px; margin-right: 100px; border: 1px solid black; padding: 7px; background-color:#94a7e0; -moz-border-radius: 10px; -webkit-border-radius: 10px; border-top-left-radius:10px;">
-<?php /*
-if($_SESSION['fail'] == 1)
-{
-	echo 'Mauvaise combinaison pseudo / mot de passe, merci de r&eacute;essayer !';
-	$_SESSION['fail'] = 0;
-	echo '<br />';
-}*/
-?>
+
 <form action="check.php" method="post">
 <input type="hidden" name="ip" value="" /><br />
 Pseudo : <input type="text" name="pseudo" style="position:absolute; left: 400px;" /><br />
